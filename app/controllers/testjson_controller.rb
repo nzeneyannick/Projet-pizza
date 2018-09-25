381fbfd67d967d5a75322cd9983e55754646e14f
yannick.djopgang-nzene <yannick.djopgang-nzene@utbm.fr>
2018-09-25 10:32:35 -0400

    test travis

---
+++ app/controllers/testjson_controller.rb
@@ -0,0 +1,9 @@
+class TestjsonController < ApplicationController
+  
+  skip_before_action :verify_authenticity_token, :only => [:create]
+  
+  def create
+    puts params.inspect
+    render json: params
+  end
+end
