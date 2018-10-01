ea65430343b132bc8bd981a9ccf45267dbcdfa81
yannick.djopgang-nzene <yannick.djopgang-nzene@utbm.fr>
2018-09-30 22:54:17 -0400

    creation fichier  vue et controler pour list des commandes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :pizzas
+  post 'orders/createOrder', to: 'orders#createOrder'
+  get 'orders/showListPizzas', to: 'orders#showListPizzas'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
